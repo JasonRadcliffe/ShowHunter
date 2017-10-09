cf74c467e7670e7c6ef075efcee57d75bad5da10
EC2 Default User <ec2-user@ip-172-31-3-171.ec2.internal>
2017-10-09 18:23:55 -0400

    changed the message to be gramatically correct when showcount = 1

--- showhunter.rb
+++ showhunter.rb
@@ -7,7 +7,7 @@ require 'mailgun'
 def send_email(emailBody)
 
 	#modified api key
-  	mg_client = Mailgun::Client.new("key-297b8fb482fc51106e22e6d7c39")
+  	mg_client = Mailgun::Client.new("key-5a9297b8fb482fc51106e22e6d7c")
 
 	message_params =  {
 	                   from: 'Mailgun Sandbox <postmaster@sandboxc2496bc8b5aa4482b13eef64e32ae0c7.mailgun.org>',
@@ -30,19 +30,23 @@ showlist = [
 '13',		#The Flash
 '2790',		#The Good Place
 '5495',		#Lethal Weapon
-'1850',		#Supergirl
 '1864',		#Superstore
 '49',		#Brooklyn Nine Nine
-'9315',	 	#No Tomorrow
 '2474', 	#Life in Pieces
 '618',		#Better Call Saul
 '3104',		#Trial & Error
-'13638',	#Making History
 '66',		#The Big Bang Theory
-'17129',	#Imaginary Mary
-'2793',		#Powerless
 '44',		#Scorpion
-'143'		#Silicon Valley
+'143',		#Silicon Valley
+'32',		#Fargo
+'21845',	#The Good Doctor
+'24780',	#Kevin (Probably) Saves the World
+'20597',	#Alex, Inc.
+'28133',	#The Mayor
+'20660',	#Me, Myself & I
+'26020',	#Young Sheldon
+'69',		#The Blacklist
+'7480'		#Star Trek - Discovery
 ]
 
 showCount = 0;
@@ -108,10 +112,18 @@ showlist.each do |show|
 	end
 end
 
-emailBody.insert(0, "There were " + showCount.to_s + " shows yesterday!\n");
-puts emailBody
-send_email(emailBody)
+if (showCount == 1)
+	emailBody.insert(0, "There was 1 show yesterday!\n")
+	puts emailBody
+elsif (showCount > 1)
+	emailBOdy.insert(0, "There were " + showCount.to_s + " shows yesterday!\n")
+	puts emailBody
+end
 
+
+if (showCount != 0)
+	send_email(emailBody)
+end
 
 
 
